f290fb2539fededdc4a8de2ef9183b150d788175
Andrew Kelly <kellyaj@gmail.com>
2015-05-23 14:46:09 -0700

    adds human readable date to post

--- app/assets/javascripts/components/_timeline_post.js.jsx
+++ app/assets/javascripts/components/_timeline_post.js.jsx
@@ -1,9 +1,17 @@
 var TimelinePost = React.createClass({
+  presentDateTime: function(rawDate) {
+    var rawDate = new Date(rawDate);
+    var minutes = ('0' + rawDate.getMinutes()).slice(-2);
+    var date = (rawDate.getMonth() + 1) + "/" + rawDate.getDate() + "/" + rawDate.getFullYear() + " at " + rawDate.getHours() + ":" + minutes
+    return date;
+  },
+
   render: function() {
+    datetime = this.presentDateTime(this.props.createdAt);
     return (
       <div>
-        <h4>{this.props.authorName}:</h4>
         <p>{this.props.message}</p>
+        <h4>{this.props.authorName}, {datetime}</h4>
       </div>
     )
   }
--- app/assets/javascripts/components/_timeline_post_list.js.jsx
+++ app/assets/javascripts/components/_timeline_post_list.js.jsx
@@ -1,7 +1,7 @@
 var TimelinePostList = React.createClass({
   render: function() {
     var postNodes = this.props.posts.map(function(post) {
-      return <TimelinePost message={post.message} authorName={post.author_name} key={post.id} />
+      return <TimelinePost message={post.message} authorName={post.author_name} key={post.id} createdAt={post.created_at} />
     });
     return (
       <div className="posts-list">
