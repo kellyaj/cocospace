9a90fb19d9d5a383ef15fbc26d56e63757f77df0
Andrew Kelly <kellyaj@gmail.com>
2015-05-23 12:24:15 -0700

    serving up presenter object

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -3,8 +3,15 @@ class SpacesController < ApplicationController
     slug = params[:slug]
     if slug.nil?
       redirect_to new_space_path
+      return
     end
-    @space = Space.find_by_slug(params[:slug])
+
+    space = Space.find_by_slug(params[:slug])
+
+    @presenter = {
+      :space => space,
+      :timeline => retrieve_timeline(space)
+    }
   end
 
   def new
@@ -15,5 +22,15 @@ class SpacesController < ApplicationController
     Space.create({:name => params[:name]})
 
     redirect_to ''
+  end
+
+  private
+
+  def retrieve_timeline(space)
+    timeline = space.timeline
+    {
+      :name => timeline.name,
+      :posts => timeline.posts
+    }
   end
 end
