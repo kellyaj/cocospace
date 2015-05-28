58f3701c366c764ca2a09c626bf9bd574531aea8
Andrew Kelly <kellyaj@gmail.com>
2015-05-28 06:54:32 -0700

    basic nav styling

---
+++ app/assets/javascripts/components/_nav.js.jsx
@@ -0,0 +1,12 @@
+var Nav = React.createClass({
+
+  render: function() {
+    return (
+      <div className="nav-container">
+        <p className="nav-item">New</p>
+        <p className="nav-item">Copy Link</p>
+        <p className="nav-item">About</p>
+      </div>
+    )
+  }
+});
