ec6be05b288c237072a7cdd73153ccf5f6a33273
Andrew Kelly <kellyaj@gmail.com>
2015-08-23 17:45:39 -0700

    retrieving lifetime activities

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @profile = Services::Fitness::DataService.get_profile_data(session[:access_token])
+    @lifetime_activities = Services::Fitness::DataService.get_lifetime_activities(session[:access_token])
   end
 end
--- lib/services/fitness/data_service.rb
+++ lib/services/fitness/data_service.rb
@@ -2,8 +2,13 @@ module Services
   module Fitness
     class DataService
 
-      def self.get_profile_data(access_token)
-        url = "https://api.fitbit.com/1/user/-/profile.json"
+      def self.get_profile_data(access_token, user_id="-")
+        url = "https://api.fitbit.com/1/user/#{user_id}/profile.json"
+        Services::Fitness::Data::RetrievesResource.for(url, access_token).response
+      end
+
+      def self.get_lifetime_activities(access_token, user_id="-")
+        url = "https://api.fitbit.com/1/user/#{user_id}/activities.json"
         Services::Fitness::Data::RetrievesResource.for(url, access_token).response
       end
 
