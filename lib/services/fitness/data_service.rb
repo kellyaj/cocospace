f4a30b6b164119d79663a22f89cea923dc3117ad
Andrew Kelly <kellyaj@gmail.com>
2015-08-23 18:23:57 -0700

    refactors base url in data service

--- lib/services/fitness/data_service.rb
+++ lib/services/fitness/data_service.rb
@@ -2,33 +2,31 @@ module Services
   module Fitness
     class DataService
 
+      def self.base_url
+        "https://api.fitbit.com/1"
+      end
+
       def self.make_request(url, access_token)
         Services::Fitness::Data::RetrievesResource.for(url, access_token).response
       end
 
       def self.get_profile_data(access_token, user_id="-")
-        url = "https://api.fitbit.com/1/user/#{user_id}/profile.json"
-
-        make_request(url, access_token)
+        make_request("#{base_url}/user/#{user_id}/profile.json", access_token)
       end
 
       def self.get_lifetime_activities(access_token, user_id="-")
-        url = "https://api.fitbit.com/1/user/#{user_id}/activities.json"
-
-        make_request(url, access_token)
+        make_request("#{base_url}/user/#{user_id}/activities.json", access_token)
       end
 
       def self.todays_activities(access_token, user_id="-")
         formatted_date = format_date(Time.current.in_time_zone("America/Los_Angeles"))
-        url = "https://api.fitbit.com/1/user/#{user_id}/activities/date/#{formatted_date}.json"
+        url = "#{base_url}/user/#{user_id}/activities/date/#{formatted_date}.json"
 
         make_request(url, access_token)
       end
 
       def self.activities_for_date(access_token, date, user_id="-")
-        url = "https://api.fitbit.com/1/user/#{user_id}/activities/date/#{format_date(date)}.json"
-
-        make_request(url, access_token)
+        make_request("#{base_url}/user/#{user_id}/activities/date/#{format_date(date)}.json", access_token)
       end
 
       private
