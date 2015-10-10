9918385e2e714391af2ad3289d68551793151144
Andrew Kelly <kellyaj@gmail.com>
2015-10-10 10:41:56 -0700

    comments out all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-  resources :spaces
-  resources :posts
-  resources :events
-  post 'login_reports' => 'login_reports#create'
-  get 'pr' => 'pull_requests#index'
-  get 'callbacks/fitbit'
-  get 'ct' => 'authorizations#fitbit'
-  get 'dashboard' => 'dashboard#index', as: "dashboard"
-  resources :tasks
-  resources :task_events
-  get '(*slug)' => 'spaces#index'
+#  resources :spaces
+#  resources :posts
+#  resources :events
+#  post 'login_reports' => 'login_reports#create'
+#  get 'pr' => 'pull_requests#index'
+#  get 'callbacks/fitbit'
+#  get 'ct' => 'authorizations#fitbit'
+#  get 'dashboard' => 'dashboard#index', as: "dashboard"
+#  resources :tasks
+#  resources :task_events
+#  get '(*slug)' => 'spaces#index'
 end
